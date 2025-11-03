b935e9fded0a0918fd85192e43738be47184946f
fuchigami <fuchigami@catchup.co.jp>
2025-11-03 13:48:15 +0900

    add コンテンツURL修復機能を実装し、管理画面の利用方法を更新

--- View/ContentsRepairs/admin/index.php
+++ View/ContentsRepairs/admin/index.php
@@ -8,34 +8,43 @@
 ?>
 <section class="section">
 	<div class="readme">
-		<h3>利用方法</h3>
-		<ul>
-			<li>1. と 2. を順番に実行してください。
+		<h2>利用方法</h2>
+		<p style="margin-bottom: 3em;">「ツール」の各項目 1 と 2 と 3 を順番に実行してください。</p>
+
+		<h3>1. コンテンツ管理のデータの整合性をチェックする</h3>
+		<p style="margin-bottom: 2em;">実行することで app/tmp/logs/log_contents_repair.log にチェック結果が記録されます。</p>
+
+		<h3>2. コンテンツ管理のツリー構造を修復する</h3>
+		<p>実行することで、コンテンツ管理のデータに対して修復を試みます。</p>
+
+		<ul style="margin-bottom: 2em;">
+			<li><strong>実行前に、必ずDBのバックアップを取得してください。</strong></li>
+			<li>実行には時間がかかる場合があります。実行中は他の操作を行わないでください。
 				<ul>
-			<li>1 実行することで app/tmp/logs/log_contents_repair.log にチェック結果が記録されます。</li>
-			<li>2 実行することでコンテンツ管理のデータに対して修復を試みます。
-				<ul>
-					<li><strong>2 の実行前に、必ずDBのバックアップを取得してください。</strong></li>
-					<li>2 の実行には時間がかかる場合があります。実行中は他の操作を行わないでください。
-						<ul>
-							<li>
-								実行環境により、2 の実行中にタイムアウトエラーが出る場合があります。
-								その場合、ログファイルに「終了時間」の記録があることを確認してください。
-								<br>
-								終了時間が記録されている場合は、処理は正常に終了しています。<br>
-								終了時間が記録されていない場合は、処理が途中で止まっている可能性があるため、DBバックアップから復旧してください。
-							</li>
-						</ul>
-					</li>
 					<li>
-						<strong>修復実行後、コンテンツの並び順は可能な限り以前のままとなる仕組みですが、順序の保証は不可能なため、コンテンツ一覧画面のスクショを撮っておくなどしてご確認ください。</strong>
-					</li>
-					<li>
-						ツール実行後、ゴミ箱の中身を空にしてください。
+						実行環境により、2 の実行中にタイムアウトエラーが出る場合があります。
+						その場合、ログファイルに「終了時間」の記録があることを確認してください。
+						<br>
+						終了時間が記録されている場合は、処理は正常に終了しています。<br>
+						終了時間が記録されていない場合は、処理が途中で止まっている可能性があるため、DBバックアップから復旧してください。
 					</li>
 				</ul>
 			</li>
+			<li>
+				<strong>修復実行後、コンテンツの並び順は可能な限り以前のままとなる仕組みですが、順序の保証は不可能なため、コンテンツ一覧画面のスクリーンショットを撮っておくなどしてご確認ください。</strong>
+			</li>
+
+			<?php if ($dbType === 'sqlite'): ?>
+			<li>
+				ツール実行後、ゴミ箱の中身を空にしてください。
+			</li>
+			<?php endif; ?>
 		</ul>
+
+		<h3>3. 「ユーティリティ」へ移動し「固定ページテンプレート書出」を実行する</h3>
+		<p>
+			ユーティリティへ移動したのち、「固定ページテンプレート書出」を実行してください。
+		</p>
 	</div>
 </section>
 
@@ -45,13 +54,19 @@
 			<li>1.<?php // TreeBehavior::verifyする ?>
 				<?php $this->BcBaser->link('コンテンツ管理のデータの整合性をチェックする',
 					['action' => 'verify_contents_tree'],
-					['class' => 'button exec-verify'],
+					['class' => 'button exec-verify']
 				); ?>
 			</li>
 			<li>2.<?php // TreeBehavior::reorderする ?>
 				<?php $this->BcBaser->link('コンテンツ管理のデータの整合性を修復する',
 					['action' => 'reflesh_contents', '?' => ['mode' => 'addlft']],
-					['class' => 'button exec-repair'],
+					['class' => 'button exec-repair']
+				); ?>
+			</li>
+			<li>3.<?php // PagesController::write_page_files() ?>
+				<?php $this->BcBaser->link('ユーティリティへ移動し「固定ページテンプレート書出」を実行する',
+					['controller' => 'tools', 'action' => 'index', 'plugin' => null],
+					['class' => 'button']
 				); ?>
 			</li>
 		</ul>
@@ -64,15 +79,15 @@
 		&nbsp;&nbsp;&nbsp;&nbsp;
 		<?php $this->BcBaser->link('ダウンロード',
 			['action' => 'download'],
-			['class' => 'button-small exec-download'],
+			['class' => 'button-small exec-download']
 		); ?>
 		<?php endif ?>
 	</h2>
 
 		<?php echo $this->BcForm->textarea('LogContentsRepair.log', [
 			'value' => $repairLog,
-			'style' => 'width:99%;height:300px;font-size:12px',
-			'readonly' => 'readonly',
+			'style' => 'width:99%;height:300px;font-size:12px;',
+			'readonly' => 'readonly'
 		]); ?>
 </div>
 
